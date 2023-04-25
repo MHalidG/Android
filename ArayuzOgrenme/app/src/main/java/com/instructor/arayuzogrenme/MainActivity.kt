96199584106b28ac1e6a8aaac6f0dbcd8f044560
Kirito <halid.gundz@gmail.com>
2023-04-25 20:41:16 +0200

    Kotlin 6.3

---
+++ ArayuzOgrenme/app/src/main/java/com/instructor/arayuzogrenme/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.instructor.arayuzogrenme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        //Layout Cesitleri
+
+        //1-Constrait layout
+        /*En sik kullanilan layout turu. En serbest calisma imkanini bu veriyor
+        2-Linear Layout
+        Tek cizgi hizaliyor dikey veya yatay ayarini yapmak gerekiyor
+         android:orientation="vertical" yada horizental
+
+         3-Relative layout
+         Nesneler birbiriyle iliskilendirilerek konumladniriliyor
+         rightOf: "@+id: buttom" gibi yazimlarda hangi nesnenin neresinde durcagi tanimlaniyor mesela
+
+        4-FrameLayout Tek resim kullanacagimiz zaman kullaniyoruz
+
+        5-Grid Layout
+
+
+         */
+
+    }
+}
