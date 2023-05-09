3efacef5bf457eb8fad1569a651e4de626759162
Kirito <halid.gundz@gmail.com>
2023-05-09 15:13:11 +0200

    Kotlin 8.1

--- KullaniciAdiSaklamaApp/app/src/main/java/com/instructor/kullaniciadisaklamaapp/MainActivity.kt
+++ KullaniciAdiSaklamaApp/app/src/main/java/com/instructor/kullaniciadisaklamaapp/MainActivity.kt
@@ -4,13 +4,14 @@ import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.instructor.kullaniciadisaklamaapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     //var sharedPreferences: SharedPreferences?=null
     lateinit var sharedPreferences: SharedPreferences
-    //lateinit ben bunu onCreate de tanimlayacagim hata verme lutfen durumlarinda bu kullanilir
+    var alinanKullaniciAdi:String ?=null    //lateinit ben bunu onCreate de tanimlayacagim hata verme lutfen durumlarinda bu kullanilir
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,20 +22,43 @@ class MainActivity : AppCompatActivity() {
         setContentView(view)
         //SharedPreferences
         //xml dosyasinda tutuyor veriyi ilkel bir yapi
-
-        var sharedPreferences= this.getSharedPreferences("com.instructor.kullaniciadisaklamaapp",
+        sharedPreferences= this.getSharedPreferences("com.instructor.kullaniciadisaklamaapp",
             MODE_PRIVATE)
 
+        alinanKullaniciAdi=sharedPreferences.getString("kullanici","")
+        if (alinanKullaniciAdi!=null){
+            binding.textView.text="Kaydedilen Kullanici Adi ${alinanKullaniciAdi}"
+        }
+        //binding.button.setOnClickListener {  }
 
     }
 
     fun kaydet(view : View){
-        val kullaniciAdi=binding.editText.text.toString()
-        sharedPreferences
+
+        val kullaniciAdi= binding.editText.text.toString()
+        if (kullaniciAdi==""){
+            Toast.makeText(this,"Lutfen Bir Deger Giriniz", Toast.LENGTH_LONG).show()
+        }else{
+            sharedPreferences.edit().putString("kullanici",kullaniciAdi).apply()
+            binding.textView.text="Kaydedilen Kullanici Adi: ${kullaniciAdi}"
+
+
+        }
+
+
+
 
     }
 
     fun sil(view : View){
+
+        alinanKullaniciAdi=sharedPreferences.getString("kullanici","")
+
+
+        if(alinanKullaniciAdi!=null){
+            binding.textView.text="Kaydedilen Kullanici Adi:"
+            sharedPreferences.edit().remove("kullanici").apply()
+        }
 
     }
 
