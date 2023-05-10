c5483eb3a5341ebd970d12ee05ae79b008512d8a
Kirito <halid.gundz@gmail.com>
2023-05-11 00:22:18 +0200

    11.1 Fragment Giris

---
+++ FragmentKotlin/app/src/main/java/com/instructor/fragmentkotlin/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.instructor.fragmentkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Android Jetpack FrameWork
+        /*Fragmentler aktivitelerden daha verimli.
+        Bir aktivite icinde birden fazla fragment kurulabiliyor
+        Aktivitelerden bagimsiz Fragmentlerin de kendi yasam donguleri var.*/
+
+
+
+    }
+
+}
